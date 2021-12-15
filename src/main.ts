d29078d42dbcced99d06aa5567c282a693ff5e26
Máté Kiss <34075771+leopoldus97@users.noreply.github.com>
2021-12-15 12:43:00 +0100

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,6 +15,15 @@ async function bootstrap() {
       password: process.env.MQTT_PASSWORD,
     },
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Traffic-Counter')
+    .setDescription('Traffic-Counter API')
+    .setVersion('1.0')
+    .addTag('traffic-counter')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.startAllMicroservices();
   await app.listen(process.env.PORT || 3000);
