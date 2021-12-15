9f9eb25b9e053d978c9f0445af89c6ef755038f0
Máté Kiss <34075771+leopoldus97@users.noreply.github.com>
2021-12-15 16:56:28 +0100

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+  });
+
   app.connectMicroservice({
     transport: Transport.MQTT,
     options: {
