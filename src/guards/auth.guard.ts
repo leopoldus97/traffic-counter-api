f7d8517d041b856e2589228ba785fffdeca68418
Marek Stančík <marekst11@gmail.com>
2021-12-21 16:13:53 +0100

    Refator auth guard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,27 +1,33 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, Logger } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthenticationService } from 'src/services/authentication.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  public constructor(
-    private readonly authenticationService: AuthenticationService,
-  ) {}
 
-  public async canActivate(context: ExecutionContext): Promise<boolean> {
-    const req = context.switchToHttp().getRequest() as Request;
-    const token = req.get('x-session-token');
-    try {
-      const session = await this.authenticationService.getSession(token);
+    private readonly log: Logger = new Logger(AuthGuard.name);
 
-      session.lastRequestDate = new Date();
-      await session.save();
+    public constructor(private readonly authenticationService: AuthenticationService) { }
 
-      req.session = session;
-      return true;
-    } catch (error) {
-        console.log(error);
-      return false;
+    public async canActivate(context: ExecutionContext): Promise<boolean> {
+        const req = context.switchToHttp().getRequest() as Request;
+        const token = req.get('x-session-token');
+
+        if(!token) {
+            return false;
+        }
+
+        try {
+            const session = await this.authenticationService.getSession(token);
+
+            session.lastRequestDate = new Date();
+            await session.save();
+
+            req.session = session;
+            return true;
+        } catch (error) {
+            this.log.error(error.message || error);
+            return false;
+        }
     }
-  }
 }
